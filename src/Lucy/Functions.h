e8eaa6785744a6f6e87c277ca634b254c56817f2
TheSuperKDK <sreenikalarikkal@gmail.com>
2022-04-11 15:33:25 +0530

    Yeah, I'm stupid. It does work. But FPS is down for some reason

--- src/Lucy/Functions.h
+++ src/Lucy/Functions.h
@@ -6,14 +6,11 @@
 
 namespace lf {
 	void RenderRect(Mesh* mesh, Texture *tex, const Vec3& pos, const Vec3& scale, const Vec2& uv0, const Vec2& uv1, bool enable_indexing = true);
-
 	void RenderRect(Mesh* mesh, const Vec3& pos, const Vec3& scale, const Vec3& color, bool fill = false, bool enable_indexing = true);
 
 	void RenderLine(Mesh* mesh, const std::vector<Vec3>& vertices, const Vec3& color);
-
 	void RenderLine(Mesh* mesh, const Vec3& v0, const Vec3& v1, const Vec3& color);
 
 	void RenderPoint(Mesh* mesh, const std::vector<Vec3>& vertices, const Vec3& color);
-
 	void RenderPoint(Mesh* mesh, const Vec3& v, const Vec3& color);
 }
